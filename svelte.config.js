e2e8a86a5237bd8bfd219d3a7feb1dc57e1790dd
Mr. Lin <cufoon@qq.com>
2023-05-20 00:08:27 +0800

    feat: 狠狠放开csp，使之能勉强部署到web

--- svelte.config.js
+++ svelte.config.js
@@ -19,11 +19,130 @@ const config = {
     csp: {
       mode: 'auto',
       directives: {
-        'default-src': ['unsafe-inline']
+        'default-src': [
+          'blob:',
+          'data:',
+          'filesystem:',
+          'http:',
+          'https:',
+          'localhost',
+          'localhost:*',
+          'mediastream:',
+          'self',
+          'unsafe-eval',
+          'unsafe-hashes',
+          'unsafe-inline',
+          'wasm-unsafe-eval'
+        ],
+        'script-src': [
+          'blob:',
+          'data:',
+          'filesystem:',
+          'http:',
+          'https:',
+          'localhost',
+          'localhost:*',
+          'mediastream:',
+          'self',
+          'unsafe-eval',
+          'unsafe-hashes',
+          'unsafe-inline',
+          'wasm-unsafe-eval'
+        ],
+        'img-src': [
+          'blob:',
+          'data:',
+          'filesystem:',
+          'http:',
+          'https:',
+          'localhost',
+          'localhost:*',
+          'mediastream:',
+          'self',
+          'unsafe-eval',
+          'unsafe-hashes',
+          'unsafe-inline',
+          'wasm-unsafe-eval'
+        ],
+        'media-src': [
+          'blob:',
+          'data:',
+          'filesystem:',
+          'http:',
+          'https:',
+          'localhost',
+          'localhost:*',
+          'mediastream:',
+          'self',
+          'unsafe-eval',
+          'unsafe-hashes',
+          'unsafe-inline',
+          'wasm-unsafe-eval'
+        ],
+        'script-src-elem': [
+          'blob:',
+          'data:',
+          'filesystem:',
+          'http:',
+          'https:',
+          'localhost',
+          'localhost:*',
+          'mediastream:',
+          'self',
+          'unsafe-eval',
+          'unsafe-hashes',
+          'unsafe-inline',
+          'wasm-unsafe-eval'
+        ],
+        'style-src': [
+          'blob:',
+          'data:',
+          'filesystem:',
+          'http:',
+          'https:',
+          'localhost',
+          'localhost:*',
+          'mediastream:',
+          'self',
+          'unsafe-eval',
+          'unsafe-hashes',
+          'unsafe-inline',
+          'wasm-unsafe-eval'
+        ],
+        'style-src-elem': [
+          'blob:',
+          'data:',
+          'filesystem:',
+          'http:',
+          'https:',
+          'localhost',
+          'localhost:*',
+          'mediastream:',
+          'self',
+          'unsafe-eval',
+          'unsafe-hashes',
+          'unsafe-inline',
+          'wasm-unsafe-eval'
+        ],
+        'worker-src': [
+          'blob:',
+          'data:',
+          'filesystem:',
+          'http:',
+          'https:',
+          'localhost',
+          'localhost:*',
+          'mediastream:',
+          'self',
+          'unsafe-eval',
+          'unsafe-hashes',
+          'unsafe-inline',
+          'wasm-unsafe-eval'
+        ]
       }
     },
     paths: {
-      relative: true
+      relative: false
     }
   }
 };
